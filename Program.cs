71d2a0e591c5857ff3d3db470a1368c0913880ca
Zi Liang <zi.liang@edu.sait.ca>
2024-04-02 10:11:23 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//CPRG 211 F Lab 10 - Using Recursion
+//Michael (Zi) Liang 000921925
+//Apr 2, 2024
+
+
+using CPRG211Lab10;
+
+RecursionMethods recursionMethods = new RecursionMethods();
+//simple tests
+Console.WriteLine($"Sum of integers between 2 and 9 inclusive: {recursionMethods.SumBetween(9, 2)}");
+Console.WriteLine($"Sum of integers between -10 and 100 inclusive: {recursionMethods.SumBetween(-10, 100)}");
+Console.WriteLine($"Number of times 1272 can be divided by 2: {recursionMethods.TimesDivisibleByTwo(1272)}");
+Console.WriteLine($"Number of times 1024 can be divided by 2: {recursionMethods.TimesDivisibleByTwo(1024)}");
---
+++ RecursionMethods.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPRG211Lab10
+{
+    internal class RecursionMethods
+    {
+        public int SumBetween(int m, int n)
+        {
+            //Make sure m is smaller
+            if (n < m)
+            {
+                return SumBetween(n, m);
+            }
+
+            if (m == n)
+            {
+                return m;
+            }
+            else
+            {
+                return n + SumBetween(m, n - 1);
+            }
+        }
+
+        public int TimesDivisibleByTwo(int m, int divided = 0)
+        {
+            if (m % 2 == 1)
+            {
+                return divided;
+            }
+            else
+            {
+                return TimesDivisibleByTwo(m / 2, divided + 1);
+            }
+        }
+    }
+}
